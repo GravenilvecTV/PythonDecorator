4b7f00f9ab999a267e60cc1a1b4ef2066096a261
Graven | Developpement <gravenilvec@gmail.com>
2024-09-09 18:20:35 +0200

    Add files via upload

---
+++ 1-basics.py
@@ -0,0 +1,13 @@
+
+def decorator(func):
+    def wrapper():
+        print("Avant")
+        func()
+        print("Après")
+    return wrapper
+
+@decorator
+def say_hello():
+    print("Bonjour !")
+
+say_hello()
---
+++ 2-perms.py
@@ -0,0 +1,24 @@
+
+def is_admin(func):
+    def wrapper(role):
+        if role == "admin":
+            func()
+        else:
+            print("Vous n'avez pas la permission")
+    return wrapper
+
+@is_admin
+def creer_fichier():
+    print("Fichier créer !")
+
+@is_admin
+def modifier_fichier():
+    print("Modifié supprimé !")
+
+@is_admin
+def supprimer_fichier():
+    print("Fichier supprimé !")
+
+creer_fichier("user")
+modifier_fichier("user")
+supprimer_fichier("admin")
